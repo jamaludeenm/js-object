d390da0f3f27467346a9fc9157b6fa7645ee9614
jamaludeen <justjamal17@gmail.com>
2022-08-30 02:32:57 +0530

    object

---
+++ object.js
@@ -0,0 +1,39 @@
+{
+let car={
+	name:"maserati",
+	model:"Alfieri",
+	color:"peacockblue",
+	year:2014
+}
+document.write(car.model)
+document.write("<br>")
+}
+{
+let mobile={
+	name:"samsung",
+	color:"blue",
+	price:15000
+}
+document.write(mobile.color)
+document.write("<br>")
+}
+{
+let pen={
+	name:"hero",
+	model:"m13",
+	inkcolor:"blue",
+	type:"catridge"
+}
+document.write(pen.type)
+document.write("<br>")
+}
+{
+let deliveryApp={
+	name:"swiggy",
+    time:"24HRS",
+	agents:"300",
+	deliverytype:"food"
+}
+document.write(deliveryApp.agents)
+document.write("<br>")
+}
